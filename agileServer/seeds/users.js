ab0f5a3de592b640a18cd91366b4a03548905e14
Natalie Despain <nataliedespainn@gmail.com>
2017-08-22 14:44:08 -0700

    added users seeds

--- agileServer/seeds/users.js
+++ agileServer/seeds/users.js
@@ -5,9 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {username: 'natalie', password: 'natalie', email: 'natalie@natalie.com', age: 5},
+        {username: 'thomas', password: 'thomas', email: 'thomas@thomas.com', age: 1},
+        {username: 'jesse', password: 'jesse', email: 'jesse@jesse.com', age: 999},
+        {username: 'rich', password: 'rich', email: 'rich@rich.com', age: 1000},
+        {username: 'michael', password: 'michael', email: 'michael@michael.com', age: 0}
       ]);
     });
 };
