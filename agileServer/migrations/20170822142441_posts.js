c7dd6f59d4f53346f13b697842d42bffcf237b03
jlstafford <jlowellstafford@gmail.com>
2017-08-22 14:33:37 -0700

    added knex table info

--- agileServer/migrations/20170822142441_posts.js
+++ agileServer/migrations/20170822142441_posts.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('posts', function(table) {
+    table.increments();
+    table.integer('user_id');
+    table.string('title');
+    table.string('body');
+    table.timestamps(true, true)
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('posts');
 };
