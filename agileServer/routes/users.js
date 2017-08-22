0aa65c308bd2781a9ded740c8cd8f3968b93e6a2
Natalie Despain <nataliedespainn@gmail.com>
2017-08-22 15:02:34 -0700

    added users routes

--- agileServer/routes/users.js
+++ agileServer/routes/users.js
@@ -5,7 +5,49 @@ var knex = require('../db/knex.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	knex.raw(`SELECT * FROM users`)
+	.then(function(users) {
+		res.render('users', {users: users.rows});
+	})
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  knex.raw(`SELECT * FROM users where id = ${req.params.id}`)
+	.then(function(user) {
+		res.render('edit_user', {user: user.rows[0]});
+	})
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new_user')
+});
+
+router.get('/:id', function(req, res, next) {
+  knex.raw(`SELECT * FROM users where id = ${req.params.id}`)
+	.then(function(user) {
+		res.render('show_user', {user: user.rows[0]});
+	})
+});
+
+// router.post('/', function(req, res, next) {
+//   knex.raw(`INSERT INTO users VALUES (username, password, email, age)`)
+// 	.then(function(user) {
+// 		res.redirect('/');
+// 	})
+// });
+
+// router.post('/:id', function(req, res, next) {
+//   knex.raw(`UPDATE users VALUES (username, password, email, age) WHERE id = ${req.params.id}`)
+// 	.then(function(user) {
+// 		res.redirect('/');
+// 	})
+// });
+
+router.post('/:id/delete', function(req, res, next) {
+  knex.raw(`DELETE from users WHERE id = ${req.params.id}`)
+	.then(function(user) {
+		res.redirect('/');
+	})
 });
 
 module.exports = router;
